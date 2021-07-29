b3f598751de5692e6388c79d1a00053bd7ab6ee4
Ali <ali.ahmed@berkeley.edu>
2021-07-28 19:42:34 -0700

    Update setup.sh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -13,7 +13,19 @@ mkdir -p /home/brev/.local/share/code-server/User
 cp .vscode/settings.json /home/brev/.local/share/code-server/User/settings.json
 
 ##### Your VSCode Extensions #####
-# code-server --install-extension <EXTENSION_ID>
+code-server --install-extension vsls-contrib.codetour
+code-server --install-extension ms-azuretools.vscode-docker
+code-server --install-extension mikestead.dotenv
+code-server --install-extensiondbaeumer.vscode-eslint
+code-server --install-extension golang.go
+code-server --install-extension compulim.indent4to2
+code-server --install-extension k--kato.intellij-idea-keybindings
+code-server --install-extension davidanson.vscode-markdownlint
+code-server --install-extension pkief.material-icon-theme
+code-server --install-extension ms-vscode-remote.remote-containers
+code-server --install-extension foxundermoon.shell-format
+code-server --install-extension tyriar.sort-lines
+code-server --install-extension xr0master.webstorm-intellij-darcula-theme
 
 ##### Recommended VSCode Extensions #####
 # code-server --install-extension esbenp.prettier-vscode
