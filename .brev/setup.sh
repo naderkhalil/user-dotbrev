44fda93fbc9d4b2057b983b89e9161575af15861
Ali <ali.ahmed@berkeley.edu>
2021-07-30 00:01:02 -0700

    forgot a dot

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -29,7 +29,7 @@ sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # Copying your ZSH Settings
 echo "\n" >>~/.zshrc
-cat ../zshrc >>~/.zshrc
+cat ../.zshrc >>~/.zshrc
 source ~/.zshrc
 
 ##### BASH #####
